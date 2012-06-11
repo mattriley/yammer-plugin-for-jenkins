3758bf7a22def30b8dcea3ef407755781af11885
Matthew Riley <matthew@matthewriley.name>
2012-06-11 21:27:19 +1000

    Refactor

---
+++ models/notification_sender.rb
@@ -0,0 +1,69 @@
+require 'timeout'
+require File.expand_path(File.dirname(__FILE__) + '/yammer')
+
+class NotificationSender
+
+  TIMEOUT_IN_SECONDS = 10
+
+  def initialize(build, listener, notification_data)
+    @build = build
+    @listener = listener
+    @data = notification_data
+  end
+
+  def send_notification
+    @listener.info 'Sending Yammer notification...'
+    send_notification_rescue
+  end
+
+  private
+
+  def send_notification_rescue
+    begin
+      send_notification_timeout
+    rescue TimeoutError
+      ruby_19? ? write_compatibility_issue_message : write_timeout_message
+    rescue => e
+      write_error_message e
+    end
+  end
+
+  def send_notification_timeout
+    Timeout::timeout(TIMEOUT_IN_SECONDS) { send_notification_actual }
+  end
+
+  def send_notification_actual
+    yam = Yammer.new @data.consumer_key, @data.consumer_secret, @data.oauth_key, @data.oauth_secret
+    yam.send_message message, @data.group_id
+    @listener.info 'Yammer notification sent.'
+  end
+
+  def message
+    build_result = @build.native.getResult.to_s
+    message = build_result == 'SUCCESS' ? @data.success_message : @data.unsuccessful_message
+    "#{@build.native.getFullDisplayName} #{build_result}\n#{message}"
+  end
+
+  def ruby_19?
+    jruby_version =~ /ruby-1.9/
+  end
+
+  def jruby_version
+    `jruby -v`
+  end
+
+  def write_compatibility_issue_message
+    @listener.info ['Result of Yammer notification could not be determined due to a compatibility issue with Ruby 1.9.',
+                    'For more information, see https://github.com/mattriley/yammer-plugin-for-jenkins/issues/1',
+                    jruby_version] * "\n"
+  end
+
+  def write_timeout_message
+    @listener.error 'Yammer notification timed out before result could be determined.'
+  end
+
+  def write_error_message(e)
+    @listener.error ['An unexpected error occurred while sending the Yammer notification.', e.message, e.backtrace] * "\n"
+  end
+
+end
--- models/yammer_notification.rb
+++ models/yammer_notification.rb
@@ -1,5 +1,4 @@
-require 'timeout'
-require File.expand_path(File.dirname(__FILE__) + '/yammer')
+require File.expand_path(File.dirname(__FILE__) + '/notification_sender')
 
 class YammerNotification < Jenkins::Tasks::Publisher
 
@@ -7,7 +6,7 @@ class YammerNotification < Jenkins::Tasks::Publisher
 
   display_name "Yammer Notification"
 
-  def initialize(attrs = {})
+  def initialize(attrs)
     @consumer_key = attrs['consumer_key']
     @consumer_secret = attrs['consumer_secret']
     @oauth_key = attrs['oauth_key']
@@ -17,47 +16,12 @@ class YammerNotification < Jenkins::Tasks::Publisher
     @unsuccessful_message = attrs['unsuccessful_message']
   end
 
-  ##
-  # Runs before the build begins
-  #
-  # @param [Jenkins::Model::Build] build the build which will begin
-  # @param [Jenkins::Model::Listener] listener the listener for this build.
   def prebuild(build, listener)
   end
 
-  ##
-  # Runs the step over the given build and reports the progress to the listener.
-  #
-  # @param [Jenkins::Model::Build] build on which to run this step
-  # @param [Jenkins::Launcher] launcher the launcher that can run code on the node running this build
-  # @param [Jenkins::Model::Listener] listener the listener for this build.
   def perform(build, launcher, listener)
-    listener.info 'Sending Yammer notification...'
-    message_sender = Yammer.new @consumer_key, @consumer_secret, @oauth_key, @oauth_secret
-
-    begin
-      Timeout::timeout 10 do
-        message_sender.send_message message(build), @group_id
-        listener.info 'Yammer notification sent.'
-      end
-    rescue => e
-      jruby_version = `jruby -v`
-      if jruby_version =~ /ruby-1.9/
-        listener.info 'Result of Yammer notification could not be determined due to a compatibility issue.'
-        listener.info jruby_version
-      else
-        listener.error 'Yammer notification was not sent.'
-        listener.error e.message
-        listener.error e.backtrace
-      end
-    end
-  end
-
-  private
-
-  def message(build)
-    message = build.native.getResult.to_s == 'SUCCESS' ? @success_message : @unsuccessful_message
-    "#{build.native.getFullDisplayName} #{build.native.getResult.to_s}\n#{message}"
+    sender = NotificationSender.new build, listener, self
+    sender.send_notification
   end
 
 end
